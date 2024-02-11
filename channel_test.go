d7fcf21b79e35cf6cdab0e78dd906f77479a6a97
rizal-Hartono <mastergoodtaichi@gmail.com>
2024-02-11 21:26:16 +0700

    update

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package go_routine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -75,11 +76,114 @@ func TestOutChannel(t *testing.T) {
 	channel := make(chan string)
 	defer close(channel)
 }
-func TestOutChannel2(t *testing.T) {
-	channel := make(chan string)
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
 	defer close(channel)
+
+	channel <- "insun"
+	channel <- "crypto"
+	channel <- "master"
+
+	// fmt.Println(<-channel)
+	// fmt.Println(<-channel)
+	// fmt.Println(<-channel)
+
+	fmt.Println(cap(channel))
+	fmt.Println(len(channel)) // harus tidak ditampilkan datanya agar dapat diketahui jumlah data di dalam buffer
+	fmt.Println("Selesai")
 }
-func TestOutChannel3(t *testing.T) {
-	channel := make(chan string)
+
+func TestBufferedChannel1(t *testing.T) {
+	channel := make(chan string, 3)
 	defer close(channel)
+
+	go func() {
+		channel <- "insun"
+		channel <- "crypto"
+		channel <- "master"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+	fmt.Println(cap(channel))
+	fmt.Println(len(channel))
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke" + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println(data)
+	}
+	fmt.Println("Done")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeRespon(channel1)
+	go GiveMeRespon(channel2)
+
+	counter := 0
+
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
+
+}
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeRespon(channel1)
+	go GiveMeRespon(channel2)
+
+	counter := 0
+
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		default:
+			fmt.Println("Waiting Data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
+
 }
